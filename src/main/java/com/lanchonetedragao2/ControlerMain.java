6bb0d885b9f23bcf29ac6bf72795cf8c25381626
Vitor Ávila <vitorhugosantosavila851@gmail.com>
2025-07-27 11:28:38 -0300

    FrontEnd pela metade

--- src/main/java/com/lanchonetedragao2/ControlerMain.java
+++ src/main/java/com/lanchonetedragao2/ControlerMain.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.layout.Pane;
 
 public class ControlerMain {
@@ -23,7 +24,6 @@ public class ControlerMain {
     @FXML
     private Pane pnlNAlcool;
 
-
     //Listas
     @FXML
     private ListView<String> lstSanduiche;
@@ -36,34 +36,102 @@ public class ControlerMain {
     @FXML
     private ListView<String> lstNAlcool;
 
-
     //Spinners Sanduiches
     @FXML
-    private Spinner<Integer> spnSanduiche;
+    private Spinner<Integer> spnSanduiche1;
     @FXML
-    private Spinner<Integer> spnPastel;
+    private Spinner<Integer> spnSanduiche2;
     @FXML
-    private Spinner<Integer> spnSorvete;
+    private Spinner<Integer> spnSanduiche3;
     @FXML
-    private Spinner<Integer> spnAlcool;
+    private Spinner<Integer> spnSanduiche4;
     @FXML
-    private Spinner<Integer> spnNAlcool;
-
+    private Spinner<Integer> spnSanduiche5;
+    //Spinners Pasteis
+    @FXML
+    private Spinner<Integer> spnPasteis1;
+    @FXML
+    private Spinner<Integer> spnPasteis2;
+    @FXML
+    private Spinner<Integer> spnPasteis3;
+    @FXML
+    private Spinner<Integer> spnPasteis4;
+    @FXML
+    private Spinner<Integer> spnPasteis5;
+    //Spinners Sorvetes
+    @FXML
+    private Spinner<Integer> spnSorvetes1;
+    @FXML
+    private Spinner<Integer> spnSorvetes2;
+    @FXML
+    private Spinner<Integer> spnSorvetes3;
+    @FXML
+    private Spinner<Integer> spnSorvetes4;
+    @FXML
+    private Spinner<Integer> spnSorvetes5;
+    //Spinners Alcoolicas
+    @FXML
+    private Spinner<Integer> spnAlcool1;
+    @FXML
+    private Spinner<Integer> spnAlcool2;
+    @FXML
+    private Spinner<Integer> spnAlcool3;
+    @FXML
+    private Spinner<Integer> spnAlcool4;
+    @FXML
+    private Spinner<Integer> spnAlcool5;
+    //Spinners Não Alcoolicas
+    @FXML
+    private Spinner<Integer> spnNAlcool1;
+    @FXML
+    private Spinner<Integer> spnNAlcool2;
+    @FXML
+    private Spinner<Integer> spnNAlcool3;
+    @FXML
+    private Spinner<Integer> spnNAlcool4;
+    @FXML
+    private Spinner<Integer> spnNAlcool5;
 
     // Lista de dados que será exibida
     private ObservableList<String> itensDaLista = FXCollections.observableArrayList();
 
     @FXML
     public void initialize() {
-        // Adiciona alguns dados de exemplo à lista
-        itensDaLista.add("Hamburguer de Dragão");
-        itensDaLista.add("Batatas Fritas Encantadas");
-        itensDaLista.add("Poção de Refrigerante");
-        itensDaLista.add("Sobremesa Mágica");
-        itensDaLista.add("Snack dos Aventureiros");
+        //Config Spinners Sanduiches
+        configSpinner(spnSanduiche1);
+        configSpinner(spnSanduiche2);
+        configSpinner(spnSanduiche3);
+        configSpinner(spnSanduiche4);
+        configSpinner(spnSanduiche5);
+        //Config Spinners Pasteis
+        configSpinner(spnPasteis1);
+        configSpinner(spnPasteis2);
+        configSpinner(spnPasteis3);
+        configSpinner(spnPasteis4);
+        configSpinner(spnPasteis5);
+        //Config Spinners Sorvetes
+        configSpinner(spnSorvetes1);
+        configSpinner(spnSorvetes2);
+        configSpinner(spnSorvetes3);
+        configSpinner(spnSorvetes4);
+        configSpinner(spnSorvetes5);
+        //Config Spinners Alcoolicas
+        configSpinner(spnAlcool1);
+        configSpinner(spnAlcool2);
+        configSpinner(spnAlcool3);
+        configSpinner(spnAlcool4);
+        configSpinner(spnAlcool5);
+        //Config Spinners Não Alcoolicas
+        configSpinner(spnNAlcool1);
+        configSpinner(spnNAlcool2);
+        configSpinner(spnNAlcool3);
+        configSpinner(spnNAlcool4);
+        configSpinner(spnNAlcool5);
 
-        // Associa os dados à ListView para que eles sejam exibidos
-        lstSanduiche.setItems(itensDaLista);
+    }
+    private void configSpinner(Spinner<Integer> spinner){
+        SpinnerValueFactory<Integer> spn = new SpinnerValueFactory.IntegerSpinnerValueFactory(0,20,0);
+        spinner.setValueFactory(spn);
     }
 
     //Acessar o cardápio
